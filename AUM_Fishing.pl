17e77f443d44bda0fc0e56660b6e8219f98d25ce
Arkeopix <mendiej750@gmail.com>
2014-09-04 01:26:22 +0200

    first commit

---
+++ AUM_Fishing.pl
@@ -0,0 +1,47 @@
+#! /usr/bin/perl -w
+
+use strict;
+use threads;
+use WWW::Mechanize;
+
+$ENV{ PERL_LWP_SSL_VERIFY_HOSTNAME } = 0;
+
+my $mech = WWW::Mechanize->new();
+my $url = 'https://www.adopteunmec.com';
+my $pswd = 'test123';
+my $usrn = 'mendiej@openmailbox.org';
+my $bait = 0;
+
+$mech->get( $url );
+$url = $mech->uri;
+print "$url\n";
+$mech->field( 'username', $usrn );
+$mech->field( 'password', $pswd );
+$mech->click_button( value => 'OK' );
+$url = $mech->uri;
+print "$url\n";
+
+if ( $url eq 'https://www.adopteunmec.com/index?e=login' ) {
+  print STDERR "connection failed, wrong username or password";
+  exit -1;
+}
+
+$mech->get( $url );
+
+my $res = $mech->content();
+
+#print "$res\n";
+
+my @link_match;
+if ( @link_match = $res =~ /https?:\/\/www\.adopteunmec\.com\/profile\/[0-9]+/g ) {
+  foreach my $link ( @link_match ) {
+    print "$link\n";
+
+    if ( $mech->follow_link( url => $link ) ) {
+      $bait++;
+    }
+  }
+}
+
+print "successfully baited $bait girls, now you wait for some magick mail !\n";
+exit 0;

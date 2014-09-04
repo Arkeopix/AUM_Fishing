966eb53ff775ab073b22ab81c19a16ad126f2b2d
Arkeopix <mendiej750@gmail.com>
2014-09-04 15:32:28 +0200

    some more printing

--- AUM_Fishing.pl
+++ AUM_Fishing.pl
@@ -7,30 +7,35 @@ $ENV{ PERL_LWP_SSL_VERIFY_HOSTNAME } = 0;
 
 my $mech = WWW::Mechanize->new();
 my $url = 'https://www.adopteunmec.com';
-my $pswd = 'test123';
-my $usrn = 'mendiej@openmailbox.org';
+my $pswd = 'your passwd here';
+my $usrn = 'your mail here';
 my $bait = 0;
 
 $mech->get( $url );
-print "$url\n";
+
+print "Connecting to your profile...\n";
+
 $mech->field( 'username', $usrn );
 $mech->field( 'password', $pswd );
 $mech->click_button( value => 'OK' );
 $url = $mech->uri;
-print "$url\n";
 
 if ( $url eq 'https://www.adopteunmec.com/index?e=login' ) {
   print STDERR "connection failed, wrong username or password";
   exit -1;
 }
 
+print "Connected\nfetching /home page...\n";
+
 $mech->get( $url );
 my $res = $mech->content();
+
+print "Fetched\n checking for valid profile URL...\n";
 
 my @link_match;
 if ( @link_match = $res =~ /https?:\/\/www\.adopteunmec\.com\/profile\/[0-9]+/g ) {
   foreach my $link ( @link_match ) {
-    print "$link\n";
+    print "found: $link\n";
 
     if ( $mech->get( $link ) ) {
       $bait++;

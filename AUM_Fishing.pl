c2dd6d47a7a49fe644285863ea1eb41ecf81290b
Arkeopix <mendiej750@gmail.com>
2014-09-06 08:18:41 +0200

    commit du matin wesh

--- AUM_Fishing.pl
+++ AUM_Fishing.pl
@@ -21,45 +21,36 @@ sub connect_and_fetch {
 	$mech->click_button( value => 'OK' );
 	$url = $mech->uri;
 	$mech->get( $url );
-	
+
 	if ( $url eq 'https://www.adopteunmec.com/index?e=login' ) {
 		print STDERR "connection failed, wrong username or password";
 		return undef;
-	}	
+	}
 	print "Connected\n";
-	
+
 	print "Fetching home page\n";
 	$res = $mech->content;
 	print "Fetched\n";
-	
+
 	return 1;
 }
 
 sub get_link_home_page {
 	my @link_match_home = $res =~ /https?:\/\/www\.adopteunmec\.com\/profile\/[0-9]+/g;
-	
-	foreach my $link ( @link_match_home ) {
-		print "found: $link\n";
-
-		#if ( $mech->get( $link ) ) {
-		#	$bait++;
-		#}
-	}
-	
 	return @link_match_home;
 }
 
 sub get_link_gogole {
 	my $curr_url = $mech->uri;
-	
+
 	print "$curr_url\n";
 	print "$res\n";
-	
+
 	print "Preparing to use gogole search engine\n";
 	$mech->field( 'q', $cfg{ gogole_keywords } );
 	print "coucou\n";
 	$mech->click_button( value => 'Valider' );
-	
+
 	$url = $mech->uri;
 	$mech->get( $url );
 	my $res = $mech->content;
@@ -68,13 +59,13 @@ sub get_link_gogole {
 }
 
 if ( connect_and_fetch ) {
-	my @links = ( get_link_home_page( $mech ), get_link_gogole( $mech ) );
+	my @links = ( get_link_home_page, get_link_gogole );
 	foreach my $link ( @links ) {
 		print "found: $link\n";
 
-		#if ( $mech->get( $link ) ) {
-		#	$bait++;
-		#}
+		if ( $mech->get( $link ) ) {
+			$bait++;
+		}
 	}
 
 	print "successfully baited $bait girls, now you wait for some magick mail !\n";
